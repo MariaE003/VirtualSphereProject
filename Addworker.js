30aae9f5c992b93d9a92a12beef9f592a4bfb3d2
MariaE003 <elgotbymaria@gmail.com>
2025-11-20 02:59:43 +0100

    ajouter un werker

--- Addworker.js
+++ Addworker.js
@@ -4,6 +4,9 @@ let formAdd = document.querySelector('.formAdd');
 let formAddContainer = document.querySelector('.formAddContainer');
 let AddExperienceBtn = document.querySelector('.AddExperienceBtn');
 let experienceDiv = document.querySelector('.experience')
+//side
+let workernoUnsignedSide=document.querySelector('.workernoUnsignedSide');
+let listWorkersUnsigned=document.querySelector('.listWorkers');
 
 //les champs
 let inputName = document.querySelector('.name');
@@ -13,7 +16,7 @@ let inputUrlImag = document.querySelector('.urlImag');
 
 
 //select roles 
-let selectRole=document.querySelector(".role");
+let selectRole = document.querySelector(".role");
 //pour voir images
 let imgVeiw = document.querySelector('.imgVeiw')
 let urlImag = document.querySelector('.urlImag')
@@ -28,7 +31,8 @@ let AllInputs = Array.from(document.querySelectorAll('input,textarea,select'));
 let idExperience = 0;
 
 //tableau des worker
-let worker=JSON.parse(localStorage.getItem('worker'))||[];
+let worker = JSON.parse(localStorage.getItem('worker')) || [];
+//tableau des experiences disponible
 
 
 //ouvrir fermer le modale d'jout
@@ -88,6 +92,7 @@ function AddExperience() {
     //ajouter un class et style au div
     newDiv.setAttribute('class', 'relative bg-[#9c9999] flex flex-col gap-y-3 py-7 rounded-[5px] my-3')
     newDiv.classList.add('experienceDiv');
+    newDiv.setAttribute('data-id',idExperience);
     // ajouter les champs d'experience 
     newDiv.innerHTML = `
                 <div class="divDeleteExp">
@@ -132,11 +137,13 @@ function validerForm(inputsExperience) {
     let regexPhone = /^(06|07)[0-9]{8}$/;
     let regexUrl = /^https:\/\/.+\.(jpg|png|gif)$/;
 
+    let isvalid=true;
     // les input deja exist
     AllInputs.forEach(ele => {
         if (!ele.value.trim()) {
             ele.placeholder = "le champ est obligatoir"
             ele.style.border = "1px solid red";
+            isvalid=false;
         } else {
             ele.placeholder = ""
             ele.style.border = "none";
@@ -144,21 +151,26 @@ function validerForm(inputsExperience) {
                 inputName.style.border = "red solid 1px";
                 inputName.value = "";
                 inputName.placeholder = "le champ ne doit pas cotient des chiffre"
+                isvalid=false;
             } else if (!regexEmail.test(inputEmail.value)) {
                 inputEmail.style.border = "red solid 1px";
                 inputEmail.value = "";
                 inputEmail.placeholder = "email doit contient des @gmail.com"
+                isvalid=false;
             } else if (!regexPhone.test(inputTelephone.value)) {
                 inputTelephone.style.border = "red solid 1px";
                 inputTelephone.value = "";
                 inputTelephone.placeholder = "le numero de telephone doit cotient just 10 chifres "
+                isvalid=false;
             } else if (!regexUrl.test(inputUrlImag.value)) {
                 inputUrlImag.style.border = "red solid 1px";
                 inputUrlImag.value = "";
                 inputUrlImag.placeholder = "L'URL de l'image doit commencer par https:// et se terminer par .jpg"
+                isvalid=false;
             } else {
                 ele.placeholder = ""
                 ele.style.border = "none";
+                // isvalid=true;
             }
         }
     })
@@ -166,56 +178,105 @@ function validerForm(inputsExperience) {
 
     // les input des experiance
     // console.log(inputsExperience);
-        
+
     inputsExperience.forEach(ele => {
         if (!ele.value.trim()) {
             ele.placeholder = "le champ est obligatoir"
             ele.style.border = "1px solid red";
+            isvalid=false;
         } else {
             ele.placeholder = ""
             ele.style.border = "none";
+            // isvalid=true;
         }
-    // last inchalah
-    let DatesDebut=document.querySelectorAll('input.dateDebut');
-    // console.log(DatesDebut.length);
-    
-    let DatesFin=document.querySelectorAll('input.dateFin');
-    let spanErrorDate=document.querySelectorAll('span.errorDate');
-    // console.log(spanErrorDate);
-    DatesDebut.forEach((dateDebut,i)=>{
-        let dateFin=DatesFin[i] 
-        let spanerror=spanErrorDate[i]
-        if(dateDebut.value && dateFin.value){
-            if(dateDebut.value>dateFin.value){
-                spanerror.style.cssText="color:red;text-align:center;"
-                spanerror.innerText="la date debut doit etre inferieur que la date de fin";
-            }else{
-                spanerror.style.display="none";
-                // console.log("hi");
-                
+        // last inchalah
+        let DatesDebut = document.querySelectorAll('input.dateDebut');
+        // console.log(DatesDebut.length);
+
+        let DatesFin = document.querySelectorAll('input.dateFin');
+        let spanErrorDate = document.querySelectorAll('span.errorDate');
+        // console.log(spanErrorDate);
+        DatesDebut.forEach((dateDebut, i) => {
+            let dateFin = DatesFin[i]
+            let spanerror = spanErrorDate[i]
+            if (dateDebut.value && dateFin.value) {
+                if (dateDebut.value > dateFin.value) {
+                    spanerror.style.cssText = "color:red;text-align:center;"
+                    spanerror.innerText = "la date debut doit etre inferieur que la date de fin";
+                    isvalid=false;
+                } else {
+                    spanerror.style.display = "none";
+                    // isvalid=true;
+                }
             }
-        }
+        })
     })
-    })
+    
+    if(isvalid){
+        AddWorker();
+        formAdd.reset();
+        formAddContainer.style.cssText = "display:none;"
+    }
 }
 
+//fetcher sur les roles dun worker  
 fetcherRoles()
-function fetcherRoles(){
-    fetch('./role.json').then(resp=>resp.json()).then(data=>{
-        data.roles.forEach(ele=>{
-            
-            let option=document.createElement('option');
+function fetcherRoles() {
+    fetch('./role.json').then(resp => resp.json()).then(data => {
+        data.roles.forEach(ele => {
+
+            let option = document.createElement('option');
             option.classList.add('text-black');
-            option.value=`${ele}`;
-            option.textContent=`${ele}`;
-            selectRole.appendChild(option);            
+            option.value = `${ele}`;
+            option.textContent = `${ele}`;
+            selectRole.appendChild(option);
         })
     })
 }
 
 
-function AddWorker(){
+function AddWorker() {
+    let experianceTable = [];
+    let AllExperience = Array.from(document.querySelectorAll('.experienceDiv'));
+    AllExperience.forEach((exp, i) => {
+        let jobTitle = exp.querySelector('input.jobTitle');
+        let CompanyName = exp.querySelector('input.CompanyName');
+        let dateDebut = exp.querySelector('input.dateDebut');
+        let dateFin = exp.querySelector('input.dateFin');
+        let Missions = exp.querySelector('input.Missions');
+        let Description = exp.querySelector('textarea.Description');
+
+        let experienceObject = {
+            jobTitle: jobTitle.value,
+            CompanyName: CompanyName.value,
+            dateDebut: dateDebut.value,
+            dateFin: dateFin.value,
+            Missions: Missions.value,
+            Description: Description.value,
+        }
+        experianceTable.push(experienceObject);
+
+    })
+    // console.log(experianceTable);
+    const workerObject = {
+        id:getIdWorker(),
+        name: inputName.value, email: inputEmail.value, phone: inputTelephone.value, image: inputUrlImag.value, role: selectRole.value,
+        experience: experianceTable,
+        statusWorker: 'unsigned'
+    }
+    worker.push(workerObject);
+    // console.log(worker);
+
+    addToLocalStorage(worker)
+    AfficherWorker(worker)
 
 }
-
+function getIdWorker() {
+    if(worker.length===0){
+        return 1;
+    }else{
+        return Math.max(worker.map(ele=>ele.id))+1;
+    }
+}
+// localStorage.clear();
 
